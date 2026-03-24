7513be1f4bbabf0b4adf579fbe4f205c314c9598
thanhduy2307 <truongtduy2307@gmail.com>
2026-03-24 15:50:54 +0700

    Update CoordinatorLayout.jsx

--- src/components/CoordinatorLayout.jsx
+++ src/components/CoordinatorLayout.jsx
@@ -37,7 +37,7 @@ const CoordinatorLayout = () => {
               <rect x="14" y="14" width="7" height="7" />
               <rect x="3" y="14" width="7" height="7" />
             </svg>
-            Dashboard
+            Trang chủ
           </Link>
           <Link to="/coordinator/theses" className="nav-link">
             <svg width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2">
